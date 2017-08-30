1ee719b329d235be9ed8b4ab3687ccd256bc5c46
rene paulokat <rene@so36.net>
2017-08-30 14:08:29 +0200

    test

--- src/client.js
+++ src/client.js
@@ -20,6 +20,7 @@ class Client {
   getStats(orgaSlug) {
     const init = {
       method: 'GET',
+      mode: 'cors',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
