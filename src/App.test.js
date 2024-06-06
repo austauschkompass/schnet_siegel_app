950d5969c33211aa31395a4e81ecddd61fbe2c71
Lennart Melzer <lennart.melzer@schueleraustausch.net>
2024-06-06 15:37:35 +0200

    track app to apps api
    upgrades to latest react (18)

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
+  createRoot(div).render(
     <App organisation='GYG'
          num_ratings={42}
          updated_at='2017-08-01 12:00'
          average={3.67487}
          url='http://example.com'
-    />,
-    div
+    />
   );
 });
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+import 'navigator.sendbeacon'
+import packageJson from '../package.json'
+
 import App from './App'
 import Client from './client'
-/* import registerServiceWorker from './registerServiceWorker'; */
+
+const { name: APP_NAME, version: APP_VERSION } = packageJson
+
+navigator.sendBeacon(
+  'https://api.apps.austauschkompass.de/' + APP_NAME + '/track',
+  JSON.stringify({ version: APP_VERSION, url: document.location.href })
+)
 
 const root = document.getElementById('schnet-ratings-widget')
 const client = new Client()
@@ -14,16 +23,15 @@ if (root) {
       if (res.errors) {
         console.warn(res.errors[0].title)
       } else {
-        ReactDOM.render(
+        createRoot(root).render(
             <App organisation={res.data.organisation_shortname}
                  num_ratings={res.data.num_ratings}
                  updated_at={res.data.updated_at}
                  average={res.data.average}
                  url={'https://www.schueleraustausch.net/organisationen/' + res.data.organisation_slug + '/bewertungen' }
-                 />,
-            root);
+                 />
+            );
       }
 
     })
 }
-/* registerServiceWorker(); */
