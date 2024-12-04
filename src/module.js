82c8e3575868cb6d61ff46d6f20c80e22bc98eb6
Lennart Melzer <lennart.melzer@schueleraustausch.net>
2024-12-04 11:29:48 +0100

    handle loading inside bundled component, do not bundle react (peer dep)

--- src/module.js
+++ src/module.js
@@ -1,45 +1,53 @@
 import App from './App'
 import Client from './client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const SchnetSiegelApp = () => {
+const SchnetSiegelComponent = ({ organisation }) => {
   const client = new Client({
     host: 'https://www.schueleraustausch.de',
     prefix: '/api/v2',
   })
-  const [stats, setStats] = useState({})
-  
+  const [stats, setStats] = useState()
+
   useEffect(() => {
     const fetchStats = async () => {
       try {
-      const res = await client.getStats(organisation)
+        const res = await client.getStats(organisation)
         if (res.errors) {
           console.warn(res.errors[0].title)
         } else {
           setStats(res.data)
-      }
-
+        }
       } catch (err) {
         console.error(err)
       }
     }
+
+    fetchStats()
   }, [client])
 
-  return (
-    <App
-      organisation={stats.organisation_shortname}
-      num_ratings={stats.num_ratings}
-      updated_at={stats.updated_at}
-      average={stats.average}
-      url={
-      `https://www.schueleraustausch.net/organisationen/${stats.organisation_slug}/bewertungen`
-      }
-    />
-  )
+  if (stats) {
+    return (
+      <App
+        organisation={stats.organisation_shortname}
+        num_ratings={stats.num_ratings}
+        updated_at={stats.updated_at}
+        average={stats.average}
+        url={
+        `https://www.schueleraustausch.net/organisationen/${stats.organisation_slug}/bewertungen`
+        }
+      />
+    )
+  }
+  else {
+    return (
+      <p>Loading...</p>
+    )
+  }
 }
 
 export {
   App as BareApp,
-  SchnetSiegelApp,
+  SchnetSiegelComponent,
   Client
 }
